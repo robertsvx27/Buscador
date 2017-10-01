87a3b3659113295a962f17457177068d61b6ab11
Roberto Saavedra <robertsvx@gmail.com>
2017-10-01 04:34:14 -0400

    Carga de Datos
    Carga de ciudades y Tipos

--- js/index.js
+++ js/index.js
@@ -1,52 +1,110 @@
-/*
+$(function (){
+
+    var ciudades = $("#selectCiudad");
+    var tipos = $("#selectTipo");
+    /*
   Creación de una función personalizada para jQuery que detecta cuando se detiene el scroll en la página
 */
-$.fn.scrollEnd = function(callback, timeout) {
-  $(this).scroll(function(){
-    var $this = $(this);
-    if ($this.data('scrollTimeout')) {
-      clearTimeout($this.data('scrollTimeout'));
-    }
-    $this.data('scrollTimeout', setTimeout(callback,timeout));
-  });
-};
-/*
-  Función que inicializa el elemento Slider
-*/
+    $.fn.scrollEnd = function(callback, timeout) {
+        $(this).scroll(function(){
+            var $this = $(this);
+            if ($this.data('scrollTimeout')) {
+                clearTimeout($this.data('scrollTimeout'));
+            }
+            $this.data('scrollTimeout', setTimeout(callback,timeout));
+        });
+    };
 
-function inicializarSlider(){
-  $("#rangoPrecio").ionRangeSlider({
-    type: "double",
-    grid: false,
-    min: 0,
-    max: 100000,
-    from: 200,
-    to: 80000,
-    prefix: "$"
-  });
-}
-/*
+    /*
+    Función que inicializa el elemento Slider
+    */
+    function inicializarSlider() {
+        $("#rangoPrecio").ionRangeSlider({
+            type: "double",
+            grid: false,
+            min: 0,
+            max: 100000,
+            from: 500,
+            to: 80000,
+            prefix: "$"
+        });
+    }
+
+    /*
   Función que reproduce el video de fondo al hacer scroll, y deteiene la reproducción al detener el scroll
 */
-function playVideoOnScroll(){
-  var ultimoScroll = 0,
-      intervalRewind;
-  var video = document.getElementById('vidFondo');
-  $(window)
-    .scroll((event)=>{
-      var scrollActual = $(window).scrollTop();
-      if (scrollActual > ultimoScroll){
-       video.play();
-     } else {
-        //this.rewind(1.0, video, intervalRewind);
-        video.play();
-     }
-     ultimoScroll = scrollActual;
+    function playVideoOnScroll(){
+        var ultimoScroll = 0,
+            intervalRewind;
+        var video = document.getElementById('vidFondo');
+        $(window)
+            .scroll((event)=>{
+            var scrollActual = $(window).scrollTop();
+        if (scrollActual > ultimoScroll){
+            video.play();
+        } else {
+            //this.rewind(1.0, video, intervalRewind);
+            video.play();
+        }
+        ultimoScroll = scrollActual;
     })
     .scrollEnd(()=>{
-      video.pause();
+            video.pause();
     }, 10)
-}
+    }
 
-inicializarSlider();
-playVideoOnScroll();
+    function cargarCiudades(){
+
+        $.ajax({
+            url: 'php/data.php',
+            type: 'GET',
+            data: 'tipo=ciudades',
+            success: function(data, textStatus, jqXHR) {
+                ciudades.find('option').remove();
+                ciudades.append('<option value="" selected>Elige una ciudad</option>');
+                $.each(data, function (id,value){
+                    ciudades.append('<option value="'+id+'">'+value+'</option>');
+
+                });
+                $('select').material_select();
+            },
+            error: function(data, textStatus, errorThrown) {
+                console.log('message=:' + data + ', text status=:' + textStatus + ', error thrown:=' + errorThrown);
+            }
+        });
+    }
+
+    function cargarTipos(){
+
+        $.ajax({
+            url: 'php/data.php',
+            type: 'GET',
+            data: 'tipo=tipos',
+            success: function(data, textStatus, jqXHR) {
+                tipos.find('option').remove();
+                tipos.append('<option value="" selected>Elige un tipo</option>');
+                $.each(data, function (id,value){
+                    tipos.append('<option value="'+id+'">'+value+'</option>');
+
+                });
+                $('select').material_select();
+            },
+            error: function(data, textStatus, errorThrown) {
+                console.log('message=:' + data + ', text status=:' + textStatus + ', error thrown:=' + errorThrown);
+            }
+        });
+    }
+
+
+    $(function () {
+        $('select').material_select();
+        inicializarSlider();
+        playVideoOnScroll();
+        cargarCiudades();
+        cargarTipos();
+    });
+});
+
+
+
+
---
+++ php/data.php
@@ -0,0 +1,61 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: robertsvx
+ * Date: 29/09/2017
+ * Time: 2:03
+ */
+
+if( isset($_GET['tipo']) ) {
+    switch ($_GET['tipo']){
+        case 'ciudades':
+            getCiudades();
+            break;
+        case 'tipos':
+            getTipos();
+            break;
+    }
+} else {
+    die("Solicitud no válida.");
+}
+
+
+/** @var TYPE_NAME $products */
+
+function getCiudades(){
+    $items = getData('../data-1.json');
+    $ciudades = array();
+    foreach ($items as $key => $value) {
+        array_push($ciudades,$value['Ciudad']);
+    }
+    $ciudades = array_unique($ciudades);
+    sort($ciudades);
+
+    header('Content-type: application/json; charset=utf-8');
+    echo json_encode($ciudades,JSON_FORCE_OBJECT);
+}
+
+function getTipos(){
+    $items = getData('../data-1.json');
+    $tipos = array();
+    foreach ($items as $key => $value) {
+        array_push($tipos,$value['Tipo']);
+    }
+    $tipos = array_unique($tipos);
+    sort($tipos);
+
+    header('Content-type: application/json; charset=utf-8');
+    echo json_encode($tipos,JSON_FORCE_OBJECT);
+}
+
+
+function getData($dir){
+    $data_file = fopen($dir,"r");
+    $data_readed = fread($data_file, filesize($dir));
+    $data = json_decode($data_readed, true);
+    fclose($data_file);
+    return $data;
+}
+
+?>
